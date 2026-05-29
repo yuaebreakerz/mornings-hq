77f6d2476d8513b629827acf65a9d187f504b3c2
Your Nameyuaebreakerz <bboyyuaemcvall@gmail.com>
2026-05-29 22:33:20 +0700

    update website

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './hooks/useAuth';
 import { NotificationProvider } from './components/NotificationProvider';
 import { ProtectedRoute } from './components/ProtectedRoute';
